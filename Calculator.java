aa1b546c0f71e43fdeeae7fdbe136e5561e446c8
Pannawat Kingkaew <s6201012620163@email.kmutnb.ac.th>
2020-08-27 09:28:44 +0700

    create frame

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Calculator
+{
+	public static void main(String[] args)
+	{
+		JFrame frame  = new JFrame();
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setTitle("Calculator naja");
+	}
+}
