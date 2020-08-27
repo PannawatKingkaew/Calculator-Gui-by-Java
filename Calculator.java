452bf34ce3087bea66fa6ce8d069d51be176558b
Pannawat Kingkaew <s6201012620163@email.kmutnb.ac.th>
2020-08-27 09:54:38 +0700

    update displat

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator
 {
 	public static void main(String[] args)
 	{
+		//frame
 		JFrame frame  = new JFrame();
 		frame.setSize(500,500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
 		frame.setTitle("Calculator naja");
+
+		//display
+		JPanel display = new JPanel();
+		display.setLayout(null);
+		frame.add(display);
+
+		JTextField caltext = new JTextField(40);
+		caltext.setBounds(10,10,480,50);
+		display.add(caltext);
+		
+		//button
+		JButton button
+		frame.setVisible(true);
 	}
 }
